2ee2bd85a966009afd92024d63b3478b7e42e63e
krasjbee <zloyjb@gmail.com>
2021-06-21 10:39:19 +0700

    feat: next button is disabled when nothing is checked

--- app/src/main/java/com/rsschool/quiz/QuestionFragment.kt
+++ app/src/main/java/com/rsschool/quiz/QuestionFragment.kt
@@ -2,27 +2,28 @@ package com.rsschool.quiz
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioButton
 import androidx.core.os.bundleOf
-import androidx.core.view.*
+import androidx.core.view.get
+import androidx.core.view.isVisible
+import androidx.core.view.size
 import androidx.fragment.app.Fragment
 import com.rsschool.quiz.databinding.FragmentQuizBinding
 
 class QuestionFragment() : Fragment() {
     private var _binding: FragmentQuizBinding? = null
     private val binding get() = _binding!!
-    private var router:Router?=null
-    private var accumulator:AnswerAccumulator?=null
+    private var router: Router? = null
+    private var accumulator: AnswerAccumulator? = null
 
     override fun onAttach(context: Context) {
-        if (context is Router){
+        if (context is Router) {
             router = context
         }
-        if (context.applicationContext is AnswerAccumulator){
+        if (context.applicationContext is AnswerAccumulator) {
             accumulator = context.applicationContext as AnswerAccumulator
         }
         super.onAttach(context)
@@ -35,7 +36,11 @@ class QuestionFragment() : Fragment() {
     ): View {
         _binding = FragmentQuizBinding.inflate(inflater)
         val view = binding.root
-
+        val position = arguments?.getInt("position", -1)!!
+        when (position.rem(2)) {
+            1 -> activity?.setTheme(R.style.Theme_Quiz_First)
+            0 -> activity?.setTheme(R.style.Theme_Quiz_Second)
+        }
         //TODO theme changer
         return view
     }
@@ -43,35 +48,36 @@ class QuestionFragment() : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val question = arguments?.getSerializable("question") as Question
         val state = arguments?.getInt("state", -1)
-        val position = arguments?.getInt("position",-1)!!
+        val position = arguments?.getInt("position", -1)!!
         val questionText = question.questionText
         val answers = question.answers
         val correctAnswer = question.correctAnswerNumber
 
 
-
-        context?.applicationContext
         binding.nextButton.setOnClickListener {
             router?.nextFragment()
-            accumulator?.addAnswerToMap(position,binding.radioGroup.checkedRadioButtonId,)
+            accumulator?.addAnswerToMap(position, binding.radioGroup.checkedRadioButtonId)
             accumulator?.printMap()
 
         }
         binding.previousButton.setOnClickListener {
             router?.prevFragment()
-            accumulator?.addAnswerToMap(position,binding.radioGroup.checkedRadioButtonId,)
+            accumulator?.addAnswerToMap(position, binding.radioGroup.checkedRadioButtonId)
             accumulator?.printMap()
         }
+        binding.nextButton.isEnabled = false
         when (state) {
             FIRST_QUESTION -> binding.previousButton.isEnabled = false
             LAST_QUESTION -> binding.nextButton.text = "SUBMIT"
-            else -> {
-                binding.nextButton.isEnabled = true
-                binding.nextButton.text = "Next"
-            }
+            else -> binding.nextButton.text = "Next"
         }
-        binding.toolbar.title = "Question ${position +1}"
+        binding.toolbar.title = "Question ${position + 1}"
         binding.question.text = questionText
+
+        binding.radioGroup.setOnCheckedChangeListener { _, _ ->
+            binding.nextButton.isEnabled = true
+        }
+
         for (i in 0 until binding.radioGroup.size) {
             val radioButton: RadioButton = binding.radioGroup[i] as RadioButton
             val id = accumulator?.getSelectedAnswerId(position)
@@ -82,6 +88,7 @@ class QuestionFragment() : Fragment() {
                 radioButton.isVisible = false
             }
         }
+
         super.onViewCreated(view, savedInstanceState)
     }
 
@@ -91,23 +98,23 @@ class QuestionFragment() : Fragment() {
     }
 
     override fun onDetach() {
-        router=null
-        accumulator=null
+        router = null
+        accumulator = null
         super.onDetach()
     }
 
     companion object {
         @JvmStatic
-        fun getInstance(question: Question,position:Int): Fragment {
+        fun getInstance(question: Question, position: Int): Fragment {
             val fragment = QuestionFragment()
-            val bundle = bundleOf("question" to question,"position" to position)
+            val bundle = bundleOf("question" to question, "position" to position)
             fragment.arguments = bundle
             return fragment
         }
 
         @JvmStatic
         fun getInstance(question: Question, state: Int, position: Int): Fragment {
-            val fragment = getInstance(question,position)
+            val fragment = getInstance(question, position)
             fragment.arguments?.putInt("state", state)
             return fragment
         }
--- app/src/main/java/com/rsschool/quiz/QuestionList.kt
+++ app/src/main/java/com/rsschool/quiz/QuestionList.kt
@@ -2,10 +2,14 @@ package com.rsschool.quiz
 
 object QuestionList {
     val questions = listOf<Question>(
-        Question("300?",
-        listOf("programmista ","traktorista"),
-        1)//FIXME MAKE NORMAL QUESTION
-    , Question("qweqwe", listOf("qqq","qwe","wwww"),3)
-    , Question("FFFFF", listOf("qqq","qwe","wwww","qwe"),3)
+        Question(
+            "300?",
+            listOf("programmista ", "traktorista"),
+            1
+        )//FIXME MAKE NORMAL QUESTION
+        ,
+        Question("qweqwe", listOf("qqq", "qwe", "wwww"), 3),
+        Question("FFFFF", listOf("qqq", "qwe", "wwww", "qwe"), 3),
+        Question("Qusestioqweqwerg", listOf("qwe,wwww", "qw"), 2)
     )
 }
