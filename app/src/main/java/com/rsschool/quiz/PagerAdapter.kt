e1a9670e0ce4f68cfa7a839aa3e9bf936a762a4a
krasjbee <zloyjb@gmail.com>
2021-06-29 23:48:50 +0700

    fix: some fixes for Question.kt and PageAdapter.kt

--- app/src/main/java/com/rsschool/quiz/PagerAdapter.kt
+++ app/src/main/java/com/rsschool/quiz/PagerAdapter.kt
@@ -5,29 +5,18 @@ import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.rsschool.quiz.fragments.QuestionFragment
 import com.rsschool.quiz.fragments.ShareFragment
-import com.rsschool.quiz.model.Question
 import com.rsschool.quiz.utils.QuestionList
-import com.rsschool.quiz.utils.Themes
 
-class PagerAdapter(private val activity: FragmentActivity) : FragmentStateAdapter(activity) {
+class PagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
     //+1 is for share fragment
     override fun getItemCount(): Int = QuestionList.questions.size + 1
 
     override fun createFragment(position: Int): Fragment {
 
-        val currentQuestion: Question = try {
-            QuestionList.questions[position]
-            //catching if our array is not out of bounds (it never should for question fragments
-        } catch (e: Exception) {
-            Question.EMPTY
-        }
-
-        return when (position) {
-            itemCount - 1 -> ShareFragment()
-            else -> QuestionFragment.getInstance(currentQuestion, position).also {
-                val indexOfTheme = position.rem(10)
-                activity.setTheme(Themes.list[indexOfTheme])
-            }
+        return if (position <= QuestionList.questions.lastIndex) {
+            QuestionFragment.getInstance(QuestionList.questions[position], position)
+        } else {
+            ShareFragment()
         }
     }
 }
--- app/src/main/java/com/rsschool/quiz/model/Question.kt
+++ app/src/main/java/com/rsschool/quiz/model/Question.kt
@@ -6,10 +6,4 @@ data class Question(
     val questionText: String,
     val answers: List<String>,
     val correctAnswerNumber: Int
-) : Serializable {
-    companion object {
-        //to avoid null and safecall operators
-        val EMPTY: Question = Question("", emptyList(), 0)
-    }
-}
-
+) : Serializable
